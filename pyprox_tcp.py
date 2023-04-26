2c1f88dd33b2420615ae757acf42c30e92bbccb1
byehack <khosravim861@gmail.com>
2023-04-26 14:46:51 +0330

    NoClass

--- pyprox_tcp.py
+++ pyprox_tcp.py
@@ -10,7 +10,6 @@ import datetime
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-
 try:
     assert os.name == 'posix'
     import resource   # ( -> pip install python-resources )
@@ -19,98 +18,92 @@ try:
 except (AssertionError, ModuleNotFoundError):
     pass
 
+listen_PORT = 2500  # pyprox listening to 127.0.0.1:listen_PORT
 
-listen_PORT = 2500    # pyprox listening to 127.0.0.1:listen_PORT
-
-Cloudflare_IP = '162.159.135.42'   # plos.org (can be any dirty cloudflare ip)
+Cloudflare_IP = '162.159.135.42'  # plos.org (can be any dirty cloudflare ip)
 Cloudflare_port = 443
 
 L_fragment = 77   # length of fragments of Client Hello packet (L_fragment Byte in each chunk)
 fragment_sleep = 0.2  # sleep between each fragment to make GFW-cache full so it forget previous chunks. LOL.
 
-
-
 # ignore description below , its for old code , just leave it intact.
-my_socket_timeout = 60 # default for google is ~21 sec , recommend 60 sec unless you have low ram and need close soon
-first_time_sleep = 0.01 # speed control , avoid server crash if huge number of users flooding (default 0.1)
-accept_time_sleep = 0.01 # avoid server crash on flooding request -> max 100 sockets per second
+my_socket_timeout = 60  # default for google is ~21 sec , recommend 60 sec unless you have low ram and need close soon
+first_time_sleep = 0.01  # speed control , avoid server crash if huge number of users flooding (default 0.1)
+accept_time_sleep = 0.01  # avoid server crash on flooding request -> max 100 sockets per second
 
 
+def listen(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((host, port))
 
-class ThreadedServer:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((self.host, self.port))
+    sock.listen(128)  # up to 128 concurrent unaccepted socket queued , the more is refused untill accepting those.
+    while True:
+        client_sock, _ = sock.accept()
+        client_sock.settimeout(my_socket_timeout)
 
-    def listen(self):
-        self.sock.listen(128)  # up to 128 concurrent unaccepted socket queued , the more is refused untill accepting those.
-        while True:
-            client_sock, client_addr = self.sock.accept()
-            client_sock.settimeout(my_socket_timeout)
+        #print('someone connected')
+        time.sleep(accept_time_sleep)  # avoid server crash on flooding request
+        #avoid memory leak by telling os its belong to main program , its not a separate program , so gc collect it when thread finish
+        thread_up = threading.Thread(target=my_upstream, args=(client_sock,), daemon=True)
+        thread_up.start()
 
-            #print('someone connected')
-            time.sleep(accept_time_sleep)  # avoid server crash on flooding request
-            #avoid memory leak by telling os its belong to main program , its not a separate program , so gc collect it when thread finish
-            thread_up = threading.Thread(target=self.my_upstream, args=(client_sock,), daemon=True)
-            thread_up.start()
 
-    def my_upstream(self, client_sock):
-        backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        backend_sock.settimeout(my_socket_timeout)
+def my_upstream(client_sock):
+    backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    backend_sock.settimeout(my_socket_timeout)
 
+    try:
+        time.sleep(first_time_sleep)   # speed control + waiting for packet to fully recieve
+        data = client_sock.recv(16384)
+        #print('len data -> ',str(len(data)))
+        #print('user talk :')
+        if not data: raise Exception('cli syn close')
+        backend_sock.connect((Cloudflare_IP,Cloudflare_port))
+        thread_down = threading.Thread(target=my_downstream, args=(backend_sock, client_sock), daemon=True)
+        thread_down.start()
+        # backend_sock.sendall(data)
+        send_data_in_fragment(data, backend_sock)
+    except Exception:
+        #print('upstream : '+ repr(e) )
+        time.sleep(2) # wait two second for another thread to flush
+        client_sock.close()
+        backend_sock.close()
+        return
+
+    while True:
         try:
-            time.sleep(first_time_sleep)   # speed control + waiting for packet to fully recieve
-            data = client_sock.recv(16384)
-            #print('len data -> ',str(len(data)))
-            #print('user talk :')
-            if not data: raise Exception('cli syn close')
-            backend_sock.connect((Cloudflare_IP,Cloudflare_port))
-            thread_down = threading.Thread(target=self.my_downstream, args=(backend_sock, client_sock), daemon=True)
-            thread_down.start()
-            # backend_sock.sendall(data)
-            send_data_in_fragment(data,backend_sock)
+            data = client_sock.recv(4096)
+            if not data: raise Exception('cli pipe close')
+            backend_sock.sendall(data)
         except Exception:
-            #print('upstream : '+ repr(e) )
-            time.sleep(2) # wait two second for another thread to flush
+            time.sleep(2)
             client_sock.close()
             backend_sock.close()
             return
 
-        while True:
-            try:
-                data = client_sock.recv(4096)
-                if not data: raise Exception('cli pipe close')
-                backend_sock.sendall(data)
-            except Exception:
-                time.sleep(2)
-                client_sock.close()
-                backend_sock.close()
-                return
 
-    def my_downstream(self, backend_sock, client_sock):
+def my_downstream(backend_sock, client_sock):
+    try:
+        data = backend_sock.recv(16384)
+        if not data: raise Exception('backend pipe close at first')
+        client_sock.sendall(data)
+    except Exception:
+        time.sleep(2)
+        backend_sock.close()
+        client_sock.close()
+        return
+
+    while True:
         try:
-            data = backend_sock.recv(16384)
-            if not data: raise Exception('backend pipe close at first')
+            data = backend_sock.recv(4096)
+            if not data: raise Exception('backend pipe close')
             client_sock.sendall(data)
         except Exception:
             time.sleep(2)
             backend_sock.close()
             client_sock.close()
             return
-
-        while True:
-            try:
-                data = backend_sock.recv(4096)
-                if not data: raise Exception('backend pipe close')
-                client_sock.sendall(data)
-            except Exception:
-                time.sleep(2)
-                backend_sock.close()
-                client_sock.close()
-                return
 
 
 def send_data_in_fragment(data , sock):
@@ -126,4 +119,4 @@ def send_data_in_fragment(data , sock):
 
 
 print("Now listening at: 127.0.0.1:"+str(listen_PORT))
-ThreadedServer('',listen_PORT).listen()
+listen('', listen_PORT)
