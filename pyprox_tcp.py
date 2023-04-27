7bfc31a02f68b7591f3630da6981021bd85ae779
byehack <khosravim861@gmail.com>
2023-04-27 14:52:42 +0330

    rename

--- pyprox_tcp.py
+++ pyprox_tcp.py
@@ -21,51 +21,53 @@ accept_time_sleep = 0.01  # avoid server crash on flooding request -> max 100 so
 
 
 # Main method: lets make handshake (the only way GFW can detect) costly.
-def handshake(backend_sock, client_sock):
-    data = client_sock.recv(16384)
+def handshake(backend, client):
+    data = client.recv(16384)
     if not data: raise Exception('syn close')
-    backend_sock.connect((Cloudflare_IP, Cloudflare_port))
+
+    # connect to backend
+    backend.connect((Cloudflare_IP, Cloudflare_port))
 
     # print(f'{len(data)}B client hello recevied, lets send {L_fragment}B per {fragment_sleep} seconds to CF.')
     for i in range(0, len(data), L_fragment):
         fragment_data = data[i: i + L_fragment]
         print(f'sending {len(fragment_data)} bytes')
-        backend_sock.sendall(fragment_data)
+        backend.sendall(fragment_data)
         time.sleep(fragment_sleep)
     print('----------finish------------')
 
-    data = backend_sock.recv(16384)
+    data = backend.recv(16384)
     if not data: raise Exception('syn-ack close')
-    client_sock.sendall(data)
+    client.sendall(data)
     # print(f'{len(data)}B hello response moved to client.')
 
 
-def stream(recv_sock, send_sock):
+def stream(reader, writer):
     try:
         while True:
-            data = recv_sock.recv(4096)
+            data = reader.recv(4096)
             if not data: raise Exception('pipe close')
-            send_sock.sendall(data)
+            writer.sendall(data)
     except Exception as e:
         # print(f'{threading.current_thread().name}: {repr(e)}')
         time.sleep(2)  # wait two second for another thread to flush
-        send_sock.close()
-        recv_sock.close()
+        writer.close()
+        reader.close()
 
 
-def tunnel(backend_sock, client_sock):
+def tunnel(backend, client):
     time.sleep(first_time_sleep)  # speed control + waiting for packet to fully recieve
 
-    try: handshake(backend_sock, client_sock)
+    try: handshake(backend, client)
     except Exception as e:
         # print(f'Handshake: {repr(e)}')
         time.sleep(2)  # wait two second for another thread to flush
-        backend_sock.close()
-        client_sock.close()
+        backend.close()
+        client.close()
 
-    thread_down = threading.Thread(target=stream, args=(backend_sock, client_sock), daemon=True, name='backend')
+    thread_down = threading.Thread(target=stream, args=(backend, client), daemon=True, name='backend')
     thread_down.start()
-    stream(client_sock, backend_sock)
+    stream(client, backend)
 
 
 def run_proxy(host, port):
@@ -76,15 +78,16 @@ def run_proxy(host, port):
 
     sock.listen(128)  # up to 128 concurrent unaccepted socket queued , the more is refused untill accepting those.
     while True:
-        client_sock, _ = sock.accept()
-        client_sock.settimeout(my_socket_timeout)
-        backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        backend_sock.settimeout(my_socket_timeout)
+        client, _ = sock.accept()
+        client.settimeout(my_socket_timeout)
+        # create backend for this connected client
+        backend = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        backend.settimeout(my_socket_timeout)
 
         # print('someone connected')
         time.sleep(accept_time_sleep)  # avoid server crash on flooding request
         # avoid memory leak by telling os its belong to main program, so gc collect it when thread finish
-        thread_up = threading.Thread(target=tunnel, args=(backend_sock, client_sock), daemon=True, name='client')
+        thread_up = threading.Thread(target=tunnel, args=(backend, client), daemon=True, name='client')
         thread_up.start()
 
 
