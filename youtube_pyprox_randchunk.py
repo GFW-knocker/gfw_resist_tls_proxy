1033465ea786e4fb6be3045470d84db337214829
GFW-knocker <123458121+GFW-knocker@users.noreply.github.com>
2023-04-28 18:07:58 +0330

    Update youtube_pyprox_randchunk.py

--- youtube_pyprox_randchunk.py
+++ youtube_pyprox_randchunk.py
@@ -14,14 +14,30 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 
-listen_PORT = 2500    # (if you change the port , you need to change port in youtube_config.json too , in section outbound -> freedom -> reverseTLS(HTTP) -> "redirect": "127.0.0.1:2500")  
-
-Google_IP = '216.239.38.120'   # ( ping google.com and put its ip here ; most of google IPs works also for youtube )
-Google_port = 443
-
-
 num_fragment = 27  # total number of chunks that ClientHello devided into (chunks with random size)
 fragment_sleep = 0.001  # sleep between each fragment to make GFW-cache full so it forget previous chunks. LOL.
+
+
+Google_IP = '216.239.38.120'   # ( www.google.com IP)
+googlevideo_IP = '74.125.98.10'
+# '180.150.1.14'  
+# '173.194.182.234'  rr5---sn-4g5e6nsz.googlevideo.com
+# '74.125.98.10'     rr5---sn-hju7enll.googlevideo.com
+# '110.50.80.143'    google Edge CDN
+# '63.64.3.14'       google Edge CDN
+# '172.253.122.100'  suggestqueries-clients6.youtube.com
+# '142.251.16.91'    youtube-ui.l.google.com , www.youtube.com
+# '134.0.218.206'    rr3.sn-vh5ouxa-hjuz.googlevideo.com
+# '172.253.122.132'  lh3.googleusercontent.com
+# '142.251.167.94'   ssl.gstatic.com
+# '142.251.16.119'   i.ytimg.com
+# '142.251.16.95'    jnn-pa.googleapis.com
+
+
+
+
+listen_PORT_google = 2500    # (if you change the port , you need to change port in youtube_config.json too , in section outbound -> freedom -> reverseTLS -> "redirect": "127.0.0.1:2500")  
+listen_PORT_youtube = 2501    # (if you change the port , you need to change port in youtube_config.json too , in section outbound -> freedom -> reverseTLS -> "redirect": "127.0.0.1:2501")  
 
 
 
@@ -33,27 +49,41 @@ accept_time_sleep = 0.01 # avoid server crash on flooding request -> max 100 soc
 
 
 class ThreadedServer(object):
-    def __init__(self, host, port):
+    def __init__(self, host, port1 , port2 ):
         self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((self.host, self.port))
+    
+        self.sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock1.bind((host, port1))
 
-    def listen(self):
-        self.sock.listen(128)  # up to 128 concurrent unaccepted socket queued , the more is refused untill accepting those.
+        self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock2.bind((host, port2))
+
+
+    def multi_listen(self):
+        thread_web = threading.Thread(target = self.listen , args = (self.sock1,Google_IP) )
+        # thread_web.daemon = True
+        thread_web.start()
+        thread_video = threading.Thread(target = self.listen , args = (self.sock2,googlevideo_IP) )
+        # thread_video.daemon = True
+        thread_video.start()
+
+
+    def listen(self , sock , remote_ip):
+        sock.listen(128)  # up to 128 concurrent unaccepted socket queued , the more is refused untill accepting those.
         while True:
-            client_sock , client_addr = self.sock.accept()                    
+            client_sock , client_addr = sock.accept()                    
             client_sock.settimeout(my_socket_timeout)
             
             #print('someone connected')
             time.sleep(accept_time_sleep)   # avoid server crash on flooding request
-            thread_up = threading.Thread(target = self.my_upstream , args =(client_sock,) )
+            thread_up = threading.Thread(target = self.my_upstream , args =(client_sock,remote_ip) )
             thread_up.daemon = True   #avoid memory leak by telling os its belong to main program , its not a separate program , so gc collect it when thread finish
             thread_up.start()
             
 
-    def my_upstream(self, client_sock):
+    def my_upstream(self, client_sock,remote_ip):
         first_flag = True
         backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         backend_sock.settimeout(my_socket_timeout)
@@ -70,7 +100,7 @@ class ThreadedServer(object):
                     #print('user talk :')
 
                     if data:                                                                    
-                        backend_sock.connect((Google_IP,Google_port))
+                        backend_sock.connect((remote_ip,443))
                         thread_down = threading.Thread(target = self.my_downstream , args = (backend_sock , client_sock) )
                         thread_down.daemon = True
                         thread_down.start()
@@ -146,8 +176,10 @@ def send_data_in_fragment(data , sock):
     print('----------finish------------')
 
 
-print ("Now listening at: 127.0.0.1:"+str(listen_PORT))
-ThreadedServer('',listen_PORT).listen()
+ThreadedServer('',listen_PORT_google,listen_PORT_youtube).multi_listen()
+print ("now listening at: 127.0.0.1:"+str(listen_PORT_google),' for web')
+print ("Now listening at: 127.0.0.1:"+str(listen_PORT_youtube),' for video')
+
 
 
 
